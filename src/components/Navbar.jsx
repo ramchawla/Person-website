c87587d109dee17539eb8322fdebdab16d1e487c
Ram <chawlaram1@gmail.com>
2024-01-26 16:40:30 -0500

    update onclick for logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
           to='/'
           className='flex items-center gap-2'
           onClick={() => {
-            setActive("");
+            setActive("/personal-website/");
             window.scrollTo(0, 0);
           }}
         >
@@ -57,7 +57,7 @@ const Navbar = () => {
               } hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={() => setActive(nav.title)}
             >
-              <a href={`personal-website/#${nav.id}`}>{nav.title}</a>
+              <a href={`#${nav.id}`}>{nav.title}</a>
             </li>
           ))}
         </ul>
@@ -87,7 +87,7 @@ const Navbar = () => {
                     setActive(nav.title);
                   }}
                 >
-                  <a href={`personal-website/#${nav.id}`}>{nav.title}</a>
+                  <a href={`#${nav.id}`}>{nav.title}</a>
                 </li>
               ))}
             </ul>
